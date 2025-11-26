ff11391ebf591203299ccac885a7dd91becd71d5
sidmillx <113720649+sidmillx@users.noreply.github.com>
2025-11-26 21:48:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -253,6 +253,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.cookie = "authToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
                 showLogin();
                 loginForm.reset();
+                // Clear transactions table
+                const transactionTableBody = document.getElementById("transactionTableBody");
+                transactionTableBody.innerHTML = "";
             } catch (e){
                 console.error("Internal Server Error during logout!");
             } 
